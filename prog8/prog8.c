e18333f7bf9b4a0aec275dacb70ce5e0319606ed
Kartikey Rana <rana.kartikey999@gmail.com>
2025-09-29 23:07:59 +0530

    stashing

---
+++ prog8/prog8.c
@@ -0,0 +1,17 @@
+/*
+============================================================================
+Name : Q8
+Author : Kartikey Rana
+Description : Write a separate program using signal system call to catch the following signals.
+a. SIGSEGV
+b. SIGINT
+c. SIGFPE
+d. SIGALRM (use alarm system call)
+e. SIGALRM (use setitimer system call)
+f. SIGVTALRM (use setitimer system call)
+g. SIGPROF (use setitimer system call)
+Date: 29th Sep, 2025.
+============================================================================
+*/
+
+
