19a5d73f9bb73251541b82c92b9cc81ceb6bdb5e
Kartikey Rana <44106280+kartikey09@users.noreply.github.com>
2025-09-30 18:59:45 +0530

    Update prog15.c

--- prog15/prog15.c
+++ prog15/prog15.c
@@ -1,8 +1,8 @@
 /*
 ============================================================================
-Name : Q14
+Name : Q15
 Author : Kartikey Rana
-Description : Write a simple program to create a pipe, write to the pipe, read from pipe and display on
+Description : Write a simple program to send some data from parent to the child process.
 the monitor.
 Date: 30th Sep, 2025.
 ============================================================================
