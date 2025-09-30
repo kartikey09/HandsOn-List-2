6c246b8534b10571adffa939a6116e3a8ad60d32
Kartikey Rana <rana.kartikey999@gmail.com>
2025-09-30 09:33:02 +0530

    prog8

---
+++ prog9/prog9.c
@@ -0,0 +1,11 @@
+/*
+============================================================================
+Name : Q9
+Author : Kartikey Rana
+Description : Write a program to ignore a SIGINT signal then reset the default action of the SIGINT
+signal - Use signal system call.
+Date: 29th Sep, 2025.
+============================================================================
+*/
+
+
